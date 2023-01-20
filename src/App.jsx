51ade48f960b47716c0c16ab71c822429516b8c3
Hallcoder <hallcoder25@gmail.com>
2023-01-20 15:42:42 +0200

    implement the animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Navbar from "./Navbar";
 import BarDiv from "./Bar";
-import { bubbleSort } from "./utils";
+import { bubbleSort, mergeSort } from "./utils";
 function App() {
   const [bars, setBars] = useState([]);
-  const [sort,setSort] = useState("");
+  const [sort, setSort] = useState("");
   const [currentBar, setCurrentBar] = useState({});
-  const currentBarClass = ''
+  const currentBarClass = "";
   class Bar {
     height;
     width;
@@ -23,48 +23,69 @@ function App() {
     generateArray();
   }, []);
   const generateArray = () => {
+    console.log("Generating array...");
     let newBars = [];
-    let newBar; 
-    for (let i = 0; i < 15; i++) {
+    let newBar;
+    for (let i = 0; i < 20; i++) {
       newBar = new Bar(
-        Math.floor(Math.random() * 450)+20,
-        Math.ceil(Math.random() * 215)+20
+        Math.floor(Math.random() * 450) + 20,
+        Math.ceil(Math.random() * 215) + 20
       );
       newBars.push(newBar);
     }
     setBars(newBars);
   };
-  const sortStyle = (type) =>{
+  const sortStyle = type => {
     // console.log(type);
-setSort(type);
-  }
-  const visualiseSort = () =>{
+    setSort(type);
+  };
+  const visualiseSort = () => {
     let arr = bars.map(b => b.height);
-    switch(sort){
-     case "merge":
-        mergeSort(arr,setBars);
-     break;
-     case "quick":
-        quickSort(arr,setBars);
-     break;
-     case "bubble":
-            bubbleSort(arr,setBars,bars,setCurrentBar);
-     break;
-     case "insertion":
-        insertionSort(arr,setBars);
-     break;
+    switch (sort) {
+      case "merge":
+        console.log("merge");
+        mergeSort(bars, 0, bars.length, setBars, setCurrentBar);
+        break;
+      case "quick":
+        quickSort(arr, setBars);
+        break;
+      case "bubble":
+        console.log("Bubble");
+        bubbleSort(arr, setBars, bars, setCurrentBar);
+        break;
+      case "insertion":
+        insertionSort(arr, setBars);
+        break;
     }
-  }
+  };
   return (
     <div className="flex flex-col">
-      <Navbar sort={sortStyle}/>
+      <Navbar sort={sortStyle} />
       <div className="flex flex-row w-9/12 justify-around m-auto">
         {bars.map(b => {
-          return <BarDiv height={b.height} width={b.width} key={new Date().getMilliseconds() + Math.random()} id={b.id} current={b.id == currentBar.id}/>
+          return (
+            <BarDiv
+              height={b.height}
+              width={b.width}
+              key={new Date().getMilliseconds() + Math.random()}
+              id={b.id}
+              current={b.id == currentBar.id}
+            />
+          );
         })}
       </div>
-      <button className="m-auto mt-20 rounded-md bg-[#368f6a] text-white font-bold p-2" onClick={visualiseSort}>Visualise Sort</button>
-      <button className="m-auto mt-10 rounded-md bg-[#368f6a] text-white font-bold p-2">Generate Array</button>
+      <button
+        className="m-auto mt-20 rounded-md bg-[#368f6a] text-white font-bold p-2"
+        onClick={visualiseSort}
+      >
+        Visualise Sort
+      </button>
+      <button
+        className="m-auto mt-10 rounded-md bg-[#368f6a] text-white font-bold p-2"
+        onClick={generateArray}
+      >
+        Generate Array
+      </button>
     </div>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,104 @@
+function sleep(ms){
+return new Promise(resolve => setTimeout(resolve,ms))
+}
 export const  bubbleSort = async (arr, set, bars,currentBar) => {
-  console.log("Original:", arr);
   let sorted;
-  for (let i = 0; i < bars.length; i++) {
+  for (let i = 0; i < bars.length -1 ; i++) {
     sorted = true;
-    console.log("iteration", i);
-    for (let j = 0; j < bars.length - 1; j++) {
+    for(let j = 0;j < bars.length-1;j++){
       if (bars[j + 1].height < bars[j].height) {
-        currentBar(bars[j])
         sorted = false;
         let temp = bars[j + 1];
         bars[j + 1] = bars[j];
         bars[j] = temp;
-        currentBar(bars[j + 1])
         await set(bars);
-        await set(prev => [...prev, bars]);
+        currentBar(bars[j+1]);
+        await sleep(10);
       }
     }
     if (sorted) break;
   }
 };
+
+ export const mergeSort = async (arr,l,r,set,bar) => {
+  if(l < r){
+    console.log("Less than");
+    let middle = (l+r)/2;
+    mergeSort(arr,l,middle,set,bar);
+    // set(prev=> arr);
+    // await sleep(20);
+    mergeSort(arr,middle+1,r,set,bar);
+    // set(prev => arr);
+    // await sleep(20);
+    merge(arr,l,middle,r,set,bar);
+    // set(prev => arr);
+    // await sleep(20);  
+  }
+}
+const merge = async (arr,l,m,r,set,bar)=>{
+console.log("merging...");
+let left_length = m-l+1;
+let right_length = (r-m);
+let left_array = [],right_array  = [];
+//first populate the arrays
+let i = 0,j = 0;
+while(i < left_length){
+  left_array[i] = arr[i+l];
+  ++i;
+}
+while(j < right_length){
+  right_array[j] = arr[m+1+j];
+  ++j;
+}
+// if(left_array[0] == undefined) return;
+console.log("left arr:",left_array);
+console.log("right arr:",right_array);
+
+//enter values in the respective arrays by comparing them;
+i=0;
+j=0;
+let k = l;
+while(i< left_length && j < right_length){
+  console.log("we can reach here");
+  if(left_array[i].height <= right_array[j].height){
+    arr[k] = left_array[i];
+    // await bar(arr[i]);
+    // await sleep(50);
+    i++;
+  }else{
+    arr[k] = right_array[j];
+    // await bar(arr[j]);
+    // await sleep(50);
+    j++;
+  }
+  k++;
+}
+while(i < left_length){
+  arr[k] = left_array[i];
+  // await bar(arr[i]);
+  // await sleep(50);
+  k++;
+  i++;
+}
+while(j < right_length){
+  arr[k] = right_array[j];
+  // await bar(arr[j]);
+  // await sleep(50);
+  k++;
+  j++;
+}
+}
+const partition = (bars,l,r)  => {
+  let pivot = r;
+  let i = l,j
+}
+export const quickSort = (bars,l,r) => {
+if(l<r){
+  let pivot = partition(bars,l, r);
+  quickSort(bars,l,pivot);
+  quickSort(bars,pivot+1,r);
+}
+}
 const print = arr => {
   let i = 0;
   while (i < arr.length) {
